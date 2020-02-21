2f07fd8529db0a53b99287caeaa6933f436fbdc4
Alexis <alexisdavalos.tech@gmail.com>
2020-02-21 12:11:12 -0500

    Created Projects, Tasks, Resources, Project_Resources Tables

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/file/oldamerica.db3',
+      filename: './data/education.db3',
     },
     migrations: {
       directory: './data/migrations',
